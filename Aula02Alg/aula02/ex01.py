d989555b062cfe5969fd14476d6aca4ea9451f09
Gustavo Castro <gustavocastrocs@gmail.com>
2020-09-14 22:55:06 -0300

    Aula 02

---
+++ Aula02Alg/aula02/ex01.py
@@ -0,0 +1,19 @@
+#  Seu salário atual é de R$ 6500 reais. Faça um programa que calcule o novo salário com um aumento de 5%
+salario_atual = 6.500
+salario_novo = salario_atual * 1.05
+print(salario_novo)
+
+# Escreva um programa que exiba seu nome na tela
+nome = input("Informe seu nome: ")
+print(f'Bem vindo {nome}')
+
+# Calcule a soma de tres variaves
+var1 = int(input('Digite o primeiro numero: '))
+var2 = int(input('Digite o segundo numero: '))
+var3 = int(input('Digite o terceiro numero: '))
+soma = var1 + var2 + var3
+print(f'A soma de {var1} + {var2} + {var3} é igual a: {soma}')
+
+# O que acontece se eu colocar textos nas tres variaveis anteriores
+# R: Ele ira concatenar os valores.
+
---
+++ Aula02Alg/aula02/variaveis_entrada_saida.py
@@ -0,0 +1,41 @@
+import math as m
+# Inteiro (numeros inteiros positivos e negativos);
+varint = 23
+varint2 = -45
+print(varint)
+print(type(varint))
+
+# Reais (numeros decimais/reais positivos e negativos)
+varreal = 34.0
+varreal2 = -746739389893.35
+print(varreal)
+print("%.2f" %varreal2)
+print(type(varreal2))
+
+# Logicos/Booleanos (True ou False)
+varbool = True
+varbool2 = False
+print(type(varbool))
+
+# Cadeias de caracteres ou Strings ("ssasdsaasfasdsda32432%%%32")
+varstr = "sasdsdsas"
+varstr2 = '@#@#@#@ shaushasa'
+print(varstr2)
+print(type(varstr2))
+
+# Operadores +, -, *, / ,  // (DIV sem casa decimal), % (MOD: Retorna o resto da divisao), ** (POT: Potenciacao)
+print( 45 + 67)
+print( 2 ** 100)
+print( 16 ** 0.5)
+print ( 15 // 4)
+
+# Gera como saida um dado do tipo "cadeia de caracteres"
+nome = input("Informe seu primeiro nome: ")
+print(nome)
+
+# Funcoes conversao: int(), float(), boo() e srt()
+str_idade = input("Informe sua idade: ")
+idade = int(str_idade)
+print(idade)
+print(type(idade))
+
