3b35c98186e3606391b32f201af48deab6fdf20a
Danilo Vasconcelos <36779661+dannslima@users.noreply.github.com>
2020-10-24 15:57:20 -0300

    exercicio 28 do livro
    exercicio 28 do livro

--- exercicios2/exercicio22.py
+++ exercicios2/exercicio22.py
@@ -2,3 +2,12 @@
 # Construa um algoritmo que calcule o valor dos dez primeiros termos da série H, em que:
 # H = 1/pot(1,3) - 1/pot(3,3) + 1/pot(5,3) - 1/pot(7,3) + 1/pot(9,3)
 
+termo = int(input('Digite o termo '))
+i =0
+while i <= 10 :
+    if i % 2 == 0:
+        print('1/pot{},{} + '.format(termo, (3)), end=' ' )
+    else:
+        print('1/pot{},{} - '.format(termo, (3)), end=' ')
+    i += 1
+    termo+=2
