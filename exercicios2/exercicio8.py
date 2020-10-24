a2317feabf5a89f0b67c74c308fdd3f2dd8fd593
Danilo Vasconcelos <36779661+dannslima@users.noreply.github.com>
2020-10-24 12:10:15 -0300

    exercicio14 livro
    exercicio14 livrop

--- exercicios2/exercicio8.py
+++ exercicios2/exercicio8.py
@@ -1,3 +1,13 @@
-# Exercicio 14 PG-64
-# Elabore um algoritmo que obtenha o máximo divisor comum (MDC) entre dois números fornecidos.
+#elabore um algoritmo que obtenha o maximo divisor comum entre dois numeros fornecidos.
+numero1 = int(input('Digite o numero 1 '))
+numero2 =   int(input('Digite o numero 2 '))
 
+def mdc(a,b):
+    if a< b:
+        return mdc(b,a)
+    if b==0:
+        return a
+    r = a % b
+    return mdc(b,r)
+
+print(mdc(numero1, numero2))
