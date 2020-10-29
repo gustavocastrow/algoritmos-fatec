086b9a7887f24e3ce8036abda162563b211ffac9
Gustavo Castro <gustavocastrocs@gmail.com>
2020-10-28 22:07:10 -0300

    finalizado

--- exercicios2/exercicio20.py
+++ exercicios2/exercicio20.py
@@ -1,8 +1,10 @@
 # Exercicio 26 PG-65
 
-termo = int(input('Digite o termo '))
-i = 0
-while i <= 10:
-    print('{}/{} + '.format(termo, (termo*termo)), end=' ' )
-    i += 1
-    termo += 1
+s = 1
+flag = True
+
+for i in range (2, 11):
+    s = s - i/(i**2) if flag else s + i/(i**2)
+
+    flag = not flag
+print(f'Resultao de S : {s}')
